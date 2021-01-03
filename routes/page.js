3c6a389257739e0cc5abbe33d13cac868435101d
MinukBak <minukbak@gmail.com>
2021-01-03 14:12:39 +0900

    라우터 작업

--- routes/page.js
+++ routes/page.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.locals.user = null;
+  next();
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내 정보 - SimpleBlog' });
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', { title: '회원가입 - SimpleBlog' });
+});
+
 router.get('/', (req, res, next) => {
   const posts = [];
   res.render('main', {
