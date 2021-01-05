70c59c2eba6c502948e2e29c2787957d893ec8a5
MinukBak <minukbak@gmail.com>
2021-01-05 15:05:19 +0900

    게시글 작성 구현

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const { isLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+
+
+
+module.exports = router;
