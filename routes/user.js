07df93ac83c9d2a1a8cf008bf0dfafcbac2843c4
MinukBak <minukbak@gmail.com>
2021-02-03 22:22:09 +0900

    프로필 수정

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,16 @@ const User = require('../models/user');
 const router = express.Router();
 
 
-
+router.post('/profile', async (req, res, next) => {
+  try {
+    await User.update({ nick: req.body.nick }, {
+      where: {id: req.user.id },
+    });
+    res.redirect('/profile');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
